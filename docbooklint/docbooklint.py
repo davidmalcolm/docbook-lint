83ed8273baeefde9d83143b791957d0969eee81a
Dave Malcolm <dmalcolm@redhat.com>
2008-04-01 12:01:17 -0400

    Update copyright to 2008

--- docbooklint/docbooklint.py
+++ docbooklint/docbooklint.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2006 Red Hat, Inc. All rights reserved. This copyrighted material 
+# Copyright (c) 2008 Red Hat, Inc. All rights reserved. This copyrighted material 
 # is made available to anyone wishing to use, modify, copy, or 
 # redistribute it subject to the terms and conditions of the GNU General 
 # Public License v.2.
